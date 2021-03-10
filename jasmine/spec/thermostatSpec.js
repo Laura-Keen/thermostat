36dca01b9768f498ff82947f30ff74b1ca5ba6ca
Laura <keenlaura@hotmail.co.uk>
2021-03-10 15:48:34 +0000

    Test written for decrease method

--- jasmine/spec/thermostatSpec.js
+++ jasmine/spec/thermostatSpec.js
@@ -7,24 +7,29 @@ describe ('Thermostat', function() {
   beforeEach(function() {
     thermostat = new Thermostat();
     thermostat.start();
-  });
+  })
 
   describe ('starts the thermostat', function() {
 
     it('checks if thermostat is on', function() {
       expect(thermostat.getIsOn()).toBe(true);
-    });
+    })
 
     it('checks that thermostat is at 20 degrees', function() {
       expect(thermostat.getTemperature()).toEqual(20);
-    });
-  });
+    })
+  })
 
   describe ('changes temperature', function() {
     it('should increase the temperature', function() {
       thermostat.increase(5);
       expect(thermostat.getTemperature()).toEqual(25);
-    });
+    })
 
-  });
-});
+    it('should decrease the temperature', function() {
+      thermostat.decrease(5);
+      expect(thermostat.getTemperature()).toEqual(15)
+    })
+
+  })
+})
